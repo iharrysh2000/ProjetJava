dee5cb772d6181c7e69535910fc29efc8ed1fb1f
penserprintemps <43381918+penserprintemps@users.noreply.github.com>
2019-03-23 11:17:13 +0100

    ..........

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+/**
+ * Représente l'application.
+ *
+ * @version  alpha 0.0
+ * @author   me
+ *
+ */
+enum Main {
+	ENVIRONNEMENT;
 
-public class Main {
-
-	public static void main(String[] args) {
+	/*
+	 * Méthode principale du programme.
+	 * @param args void
+	 */
+	public void run(String[] args) {
 		Jeu jeu = new Jeu(20);
 		if(jeu.getPiece(5, 5) != null)
 		{
@@ -10,5 +21,13 @@ public class Main {
 			a.deplacerPiece(1,1);
 			System.out.println(a.getLigne() + " " + a.getColonne());
 		}
+	}
+
+	/*
+	 * Point d'entrée du programme.
+	 * @param args les void
+	 */
+	public static void main(String[] args) {
+		ENVIRONNEMENT.run(args);
 	}
 }
