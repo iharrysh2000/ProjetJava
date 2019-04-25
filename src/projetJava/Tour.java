50b97a85b40351df07b8a5caf11eab7fab161470
TheoRigolo <48331750+TheoRigolo@users.noreply.github.com>
2019-04-26 00:23:40 +0200

    Update Tour.java

--- src/projetJava/Tour.java
+++ src/projetJava/Tour.java
@@ -1,19 +1,33 @@
 package projetJava;
 
-
+/**
+ * Sous-classe Tour représentant le type de pièce Tour
+ */
 public class Tour extends Piece{
 	
 	private final String nom = "T";
 	
+	/**
+	 * Constructeur de la tour
+	 * @param couleur la couleur de la tour
+	 */
 	public Tour(int couleur) {
 		super(couleur);
 	}
 	
+	/**
+	 * Retourne la chaine de caractères composé du nom et de la couleur de la tour
+	 * @return le nom et la couleur du fou
+	 */
 	@Override
 	public String toString () {
 		return this.nom + this.getCoul();
 	}
 	
+	/**
+	 * Déplacement du fou
+	 * @return boolean Retourne vrai si le déplacement est valide ou faux si le déplacement n'est pas valide
+	 */
 	@Override
 	public boolean movePiece (int depX, int depY, int finX, int finY) {
 		if( (depX == finX) 
