42de923f3d737f09111f209a4f8c30ee4b0fd712
TheoRigolo <48331750+TheoRigolo@users.noreply.github.com>
2019-04-26 00:24:55 +0200

    Update Roi.java

--- src/projetJava/Roi.java
+++ src/projetJava/Roi.java
@@ -1,18 +1,33 @@
 package projetJava;
 
+/**
+ * Sous-classe Roi qui correspond au type de pièce Roi
+ */
 public class Roi extends Piece{
 	
 	private final String nom = "R";
-
+	
+	/**
+	 * Constructeur du roi
+	 * @param couleur la couleur du roi
+	 */
 	public Roi(int couleur) {
 		super(couleur);
 	}
 	
+	/**
+	 * Retourne la chaine de caractères composé du nom et de la couleur du roi
+	 * @return le nom et la couleur du fou
+	 */
 	@Override
 	public String toString () {
 		return this.nom + this.getCoul();
 	}
-
+	
+	/**
+	 * Déplacement du roi
+	 * @return boolean Retourne vrai si le déplacement est valide ou faux si le déplacement n'est pas valide
+	 */
 	@Override
 	public boolean movePiece(int depX, int depY, int finX, int finY) {
 		int dx = depX - finX;
