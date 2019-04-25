b4fe538182ede54392d58717834cb8f24520cad5
TheoRigolo <48331750+TheoRigolo@users.noreply.github.com>
2019-04-26 00:56:47 +0200

    Update Main.java

--- src/projetJava/Main.java
+++ src/projetJava/Main.java
@@ -1,7 +1,14 @@
 package projetJava;
 
+/**
+ * Classe Main qui est le programme principal
+ */
 public class Main {
 
+	/**
+	 * programme principal
+	 * @param args on ne s'en sert pas
+	 */
 	public static void main(String[] args) {
 
 		Jeu jeu = new Jeu ();
