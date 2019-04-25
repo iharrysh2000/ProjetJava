bbd4a52010d5143147eb79561537c484ae87c9ff
TheoRigolo <48331750+TheoRigolo@users.noreply.github.com>
2019-04-26 00:30:11 +0200

    Update Point.java

--- src/projetJava/Point.java
+++ src/projetJava/Point.java
@@ -1,23 +1,40 @@
 package projetJava;
 
+/**
+ * Classe Point qui représente les coordonées
+ */
 public class Point {
 	
 	private int x;
 	private int y;
 	
+	/**
+	 * Constructeur du point
+	 * @param x l'abscisse
+	 * @param y l'ordonnée
+	 */
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Retourne l'ordonnée du point
+	 */ 
 	public int getY() {
 		return this.y;
 	}
 	
+	/**
+	 * Retourne l'abscisse du point
+	 */
 	public int getX() {
 		return this.x;
 	}
 	
+	/**
+	 * Retourne la chaine de caractères composée de l'abscisse et de l'ordonnée du point
+	 */
 	@Override
 	public String toString() {
 		return "x : " + this.x + ", y : " + this.y;
