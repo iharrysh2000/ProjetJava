7ece4c8d6fc055ccb7784db40a8697914a6a94fc
TheoRigolo <48331750+TheoRigolo@users.noreply.github.com>
2019-04-26 00:07:27 +0200

    Update Fou.java

--- src/projetJava/Fou.java
+++ src/projetJava/Fou.java
@@ -1,19 +1,33 @@
 package projetJava;
 
-
+/**
+ * Sous-classe Fou qui correspond au type de pièce Fou
+ */
 public class Fou extends Piece{
 	
 	private final String nom = "F";
 	
+	/**
+	 * Constructeur du fou
+	 * @param couleur la couleur du fou
+	 */
 	public Fou(int couleur) {
 		super(couleur);
 	}
 	
+	/**
+	 * Retourne la chaine de caractères composé du nom et de la couleur du fou
+	 * @return le nom et la couleur du fou
+	 */
 	@Override
 	public String toString () {
 		return this.nom + this.getCoul();
 	}
 	
+	/**
+	 * Déplacement du fou
+	 * @return boolean Retourne vrai si le déplacement est valide ou faux si le déplacement n'est pas valide
+	 */
 	@Override
 	public boolean movePiece (int depX, int depY, int finX, int finY) {
 		int dx = depX - finX;
