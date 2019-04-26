b64e9993eeed44bbffd23809238921e0f70d0201
TheoRigolo <48331750+TheoRigolo@users.noreply.github.com>
2019-04-26 07:52:50 +0200

    Update Cavalier.java

--- src/projetJava/Cavalier.java
+++ src/projetJava/Cavalier.java
@@ -3,42 +3,49 @@ package projetJava;
 /**
  * Sous-classe Cavalier qui correspond au type de pièce Cavalier
  */
-public class Cavalier extends Piece{
-	
+public class Cavalier extends Piece {
+
 	private final String nom = "C";
-	
+
 	/**
 	 * Constructeur du cavalier
+	 * 
 	 * @param couleur la couleur du cavalier
 	 */
-	public Cavalier (int couleur) {
+	public Cavalier(int couleur) {
 		super(couleur);
 	}
-	
+
 	/**
-	 * Retourne la chaine de caractères composé du nom et de la couleur du cavalier
+	 * Retourne la chaine de caractères composée du nom et de la couleur du cavalier
+	 * 
 	 * @return le nom et la couleur du cavalier
 	 */
 	@Override
-	public String toString () {
+	public String toString() {
 		return this.nom + this.getCoul();
 	}
-	
+
 	/**
 	 * Déplacement du cavalier
-	 * @return boolean Retourne vrai si le déplacement est valide ou faux si le déplacement n'est pas valide
+	 * 
+	 * @param depx l'abscisse de départ
+	 * @param depy l'ordonée de départ
+	 * @param finX l'abscisse d'arrivée
+	 * @param finy l'ordonée de d'arrivée
+	 * @return vrai si le déplacement est valide ou faux si le déplacement n'est pas
+	 *         valide
 	 */
 	@Override
 	public boolean movePiece(int depX, int depY, int finX, int finY) {
 		int dx = finX - depX;
 		int dy = finY - depY;
 		int distance = (dx >= 0 ? dx : -dx) + (dy >= 0 ? dy : -dy);
-		
-		if (distance == 3 && dx != 0 && dy != 0)
-		{
+
+		if (distance == 3 && dx != 0 && dy != 0) {
 			return true;
 		}
-			
+
 		return false;
 	}
 }
