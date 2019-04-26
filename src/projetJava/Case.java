b510cbadfddf0af19c3561291548904e34649422
TheoRigolo <48331750+TheoRigolo@users.noreply.github.com>
2019-04-26 07:49:31 +0200

    Update Case.java

--- src/projetJava/Case.java
+++ src/projetJava/Case.java
@@ -1,43 +1,47 @@
 package projetJava;
 
 /**
- * Classe Case qui associe une pièce et sa couleur à sa position
+ * Classe Case qui associe une pièce et sa couleur au plateau
  */
 public class Case {
 	private int couleur;
 	private Piece piece;
-	
+
 	/**
 	 * Constructeur d'une case
-	 * @param piece la pièce sur la case
+	 * 
+	 * @param piece   la pièce sur la case
 	 * @param couleur la couleur de la pièce
 	 */
-	public Case (Piece piece, int couleur) {
+	public Case(Piece piece, int couleur) {
 		this.piece = piece;
 		this.couleur = couleur;
 	}
-	
+
 	/**
 	 * Renvoie la pièce qui occupe la case
-	 * @return la pièce
+	 * 
+	 * @return piece la pièce
 	 */
-	public Piece getPiece () {
+	public Piece getPiece() {
 		return this.piece;
 	}
-	
+
 	/**
 	 * Renvoie la couleur de la pièce qui occupe la case
-	 * @return la couleur de la pièce
+	 * 
+	 * @return couleur la couleur de la pièce
 	 */
-	public int getCoul () {
+	public int getCoul() {
 		return this.couleur;
 	}
-	
+
 	/**
-	 * Change la pièce de la case par une autre
-	 * @param piece la pièce qui va occuper la case
+	 * Change la pièce de la case par autre chose
+	 * 
+	 * @param piece l'éventuelle pièce qui va occuper la case
 	 */
-	public void changePiece (Piece piece) {
+	public void changePiece(Piece piece) {
 		this.piece = piece;
 	}
 }
