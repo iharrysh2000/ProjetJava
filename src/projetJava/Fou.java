2b2848909349fc9ba121b48d25341fa86f93a67f
TheoRigolo <48331750+TheoRigolo@users.noreply.github.com>
2019-04-26 07:56:53 +0200

    Update Fou.java

--- src/projetJava/Fou.java
+++ src/projetJava/Fou.java
@@ -3,39 +3,45 @@ package projetJava;
 /**
  * Sous-classe Fou qui correspond au type de pièce Fou
  */
-public class Fou extends Piece{
-	
+public class Fou extends Piece {
+
 	private final String nom = "F";
-	
+
 	/**
 	 * Constructeur du fou
+	 * 
 	 * @param couleur la couleur du fou
 	 */
 	public Fou(int couleur) {
 		super(couleur);
 	}
-	
+
 	/**
-	 * Retourne la chaine de caractères composé du nom et de la couleur du fou
+	 * Retourne la chaine de caractères composée du nom et de la couleur du fou
+	 * 
 	 * @return le nom et la couleur du fou
 	 */
 	@Override
-	public String toString () {
+	public String toString() {
 		return this.nom + this.getCoul();
 	}
-	
+
 	/**
 	 * Déplacement du fou
-	 * @return boolean Retourne vrai si le déplacement est valide ou faux si le déplacement n'est pas valide
+	 * 
+	 * @param depX l'abscisse de départ
+	 * @param depY l'ordonée de départ
+	 * @param finX l'abscisse d'arrivée
+	 * @param finY l'ordonée de d'arrivée
+	 * @return vrai si le déplacement est valide ou faux si le déplacement n'est pas
+	 *         valide
 	 */
 	@Override
-	public boolean movePiece (int depX, int depY, int finX, int finY) {
+	public boolean movePiece(int depX, int depY, int finX, int finY) {
 		int dx = depX - finX;
 		int dy = depY - finY;
-		
-		if( (dx == dy) 
-				|| (dx == -dy) )
-		{
+
+		if ((dx == dy) || (dx == -dy)) {
 			return true;
 		}
 
